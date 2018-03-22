01eedde1870c089210b7292e4bce5f5c1d211bae
AeroCloud27 <34441780+AeroCloud27@users.noreply.github.com>
2018-03-22 14:51:15 +0100

    Add files via upload

--- MVC_UDEPSI/html/library/script.js
+++ MVC_UDEPSI/html/library/script.js
@@ -12,3 +12,30 @@ function topFunction() {
     document.body.scrollTop = 0; 
     document.documentElement.scrollTop = 0;
 }
+
+function isconnected(){
+
+}
+
+function display_top_formateurs(){
+
+    if (document.getElementById("top_formateurs").style.right == "0vw"){
+        document.getElementById("top_formateurs").style.right = "-15vw";
+        document.getElementById("btn_formateurs").style.width = "9vw"
+
+    } else {
+        document.getElementById("top_formateurs").style.right="0vw";
+        document.getElementById("btn_formateurs").style.width = "13vw"
+    }
+}
+
+function display_top_cours(){
+    if (document.getElementById("top_cours").style.left == "0vw"){
+        document.getElementById("top_cours").style.left = "-15vw";
+        document.getElementById("btn_cours").style.width = "9vw"
+
+    } else {
+        document.getElementById("top_cours").style.left="0vw";
+        document.getElementById("btn_cours").style.width = "13vw"
+    }
+}
