b471d6ae762c338baff51442df3dc5e822e3586e
Nigdor <36530414+Nigdor@users.noreply.github.com>
2018-03-22 16:02:27 +0100

    Update add_comp_profile.php

--- MVC_UDEPSI/controller/add_comp_profile.php
+++ MVC_UDEPSI/controller/add_comp_profile.php
@@ -5,20 +5,25 @@
  * Date: 22/03/2018
  * Time: 11:23
  */
+include_once "../model/helper.php";
 include_once "../model/udepsi.php";
 session_start();
 // On récupère l'id de la compétence que l'utilisateur a sélectionné
-$idcomp=$_GET['id_competence'];
+$idcomp=getVar('id_competence');
+$idstar=postVar('stars');
 // On récupère son id utilisateur
-$iduser=$_SESSION['id'];
+$iduser=sessionVar('id');
 
 
-if ($_GET){
+if ($_GET && $idstar<10 && $idstar>0){
     // S'il y a une id_compétence présente dans l'url alors on lie l'id compétence avec son id utilisateur
-    insertCompetenceProfile($idcomp,$iduser);
+    insertCompetenceProfile($idstar,$idcomp,$iduser);
     // On redirige l'utilisateur vers la même page avec l'extension de lien "ajoutvalide"
     header("Location: insert_comp.php?ajoutvalide");
+
     // On inclue le message d'erreur lié à cette extension de lien
     include_once "../controller/error_message.php";
 
+}else{
+    echo "impossible";
 }
